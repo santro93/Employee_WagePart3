215f9028f075a8e8b3d487aef4d66c6d089f1c93
santro93 <nsantosh435@gmail.com>
2022-03-20 15:21:16 +0530

    Calculated Wage for a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,16 +4,18 @@ public class EmployeeWage {
 	public static final int FULL_TIME = 1;
 	public static final int PART_TIME = 2;
 	public static final int EMP_WAGE_PER_HR = 20;
+	public static final int NUM_WORKING_DAYS = 20;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problem Part3!");
 		
 		
-	    int empHrs = 0;
-	    int empWage = 0; 
+	    int empHrs = 0, empWage = 0, totalEmpWage = 0;
+
+	    for (int day = 1; day <= NUM_WORKING_DAYS; day++){; 
 	    
         double empCheck = Math.floor(Math.random()*3);
-        System.out.println("Random value for emp Attendence check is: "+ empCheck);
+        System.out.println("Random value for emp Attendence check is: "+empCheck);
 
         switch ((int)empCheck){
 		        case FULL_TIME:
@@ -29,9 +31,12 @@ public class EmployeeWage {
 		        default:
 		        	System.out.println("Employee is Absent");
 		        	empHrs = 0;	
-		  }
+        	}
         empWage = empHrs*EMP_WAGE_PER_HR;
+        totalEmpWage += empWage;
         System.out.println("Employee Daily Wage is: "+empWage );
-	}     
+	 }
+	    System.out.println("Total Employee Wage is: "+totalEmpWage);	
+  }     
 }
  
