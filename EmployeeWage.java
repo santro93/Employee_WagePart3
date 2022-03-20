5f5dc3ea507afc9dc9d4087e36639dc085fb52f8
santro93 <nsantosh435@gmail.com>
2022-03-20 14:45:52 +0530

    Welcome Msg

---
+++ EmployeeWage.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.EmployeeWage_UC7;
+
+public class EmployeeWage {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Problem Part3!");
+
+	}
+}
+ 
