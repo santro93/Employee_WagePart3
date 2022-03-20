ad0c322140d234f78c446c7901fe8876b00e1a5f
santro93 <nsantosh435@gmail.com>
2022-03-20 15:12:19 +0530

    Logic by using Switch Case Statement

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,33 +1,36 @@
 package com.bridgelabz.EmployeeWage_UC7;
 
 public class EmployeeWage {
+	public static final int FULL_TIME = 1;
+	public static final int PART_TIME = 2;
+	public static final int EMP_WAGE_PER_HR = 20;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problem Part3!");
 		
-		int fullTime = 1;
-		int partTime = 2;
-		int empWagePerHr = 20;
+		
 	    int empHrs = 0;
 	    int empWage = 0; 
 	    
         double empCheck = Math.floor(Math.random()*3);
         System.out.println("Random value for emp Attendence check is: "+ empCheck);
 
-        if(empCheck == fullTime ){
-            System.out.println("Employee is Present.");
-            empHrs = 8;
-           }
-        else if((empCheck == partTime)){
-            System.out.println("Employee is present Part Time.");
-	        empHrs = 4;
-            }
-        else{
-            System.out.println("Employee is Absent.");
-            empHrs = 0;
-       		}
-        
-        empWage = empHrs*empWagePerHr;
+        switch ((int)empCheck){
+		        case FULL_TIME:
+		        	System.out.println("Employee is present Full Time");
+		        	empHrs = 8;
+		       				break;
+		       
+		        case PART_TIME:
+		       		System.out.println("Employee is present Part Time");
+		       		empHrs = 4;
+		       				break;
+		  
+		        default:
+		        	System.out.println("Employee is Absent");
+		        	empHrs = 0;	
+		  }
+        empWage = empHrs*EMP_WAGE_PER_HR;
         System.out.println("Employee Daily Wage is: "+empWage );
 	}     
 }
