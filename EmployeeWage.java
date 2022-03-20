2c0b754bb37b37f4a51414d5163f70b43912275a
santro93 <nsantosh435@gmail.com>
2022-03-20 22:15:47 +0530

    Compute Employee Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,18 +5,18 @@ public class EmployeeWage {
 	public static final int FULL_TIME = 1;
 	public static final int PART_TIME = 2;
 	public static final int EMP_WAGE_PER_HR = 20;
-	public static final int NUM_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
+	
+	
 	
 	
 	
 	//method: employee wage calculation	
-		public static void empWageComputation(){
+		public static void empWageComputation(String company, int empWagePerHr, int workingDayPerMonth, int maxHrsInMonth){
 			//variables initialization
 			 int empHrs = 0, empWage = 0, totalEmpWage = 0, totalEmpHrs = 0, totalWorkingDays = 1 ;
 
 			  //check total emp hour not more than 100 and total days not more than 20
-			    while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_WORKING_DAYS){
+			    while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays <= workingDayPerMonth){
 			    
 			        double empCheck = Math.floor(Math.random()*3);
 			        System.out.println("Random value for emp Attendence check is: "+empCheck);
@@ -37,24 +37,27 @@ public class EmployeeWage {
 					        	empHrs = 0;	
 			        	}
 			        
-		        empWage = empHrs*EMP_WAGE_PER_HR;
-		        totalEmpWage += empWage;
-		        totalEmpHrs  += empHrs;
-		        System.out.println("Employee Daily Wage is: "+empWage );
+			    //calculate total emp hours
+	            totalEmpHrs += empHrs;
+			    System.out.println("Day : "+ totalWorkingDays +" Emp Hrs : "+empHrs);
+		       
 		        
 		      //day increment by 1 every iteration
 		        totalWorkingDays++ ;
 			}
-		      System.out.println("Total Hours workdone by Employee in month is: "+ totalEmpHrs); 
-		      System.out.println("Total Employee Wage is: "+ totalEmpWage); 
+			 // calculate total emp wage
+			  totalEmpWage = totalEmpHrs * empWagePerHr;
+		      System.out.println("Total Hours workdone by Employee in month is: "+company +" " + totalEmpHrs); 
+		      System.out.println("Employee Daily Wage is: " +company +" " +totalEmpWage );
 		}
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problem Part3!");
 		
-	//method call
+		//method call
 		EmployeeWage empWageComputation = new EmployeeWage();
-		empWageComputation.empWageComputation();
+		empWageComputation.empWageComputation("D-MART", 25, 5, 25);
+		empWageComputation.empWageComputation("Vijay Sales", 25, 15, 25);
    }     
 }
  
