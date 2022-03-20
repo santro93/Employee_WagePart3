77640f60fadb1eca6536ad6bf1fb442bc076913a
santro93 <nsantosh435@gmail.com>
2022-03-20 14:54:41 +0530

    Employee Daily Wage is Calculated

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,16 +6,24 @@ public class EmployeeWage {
 		System.out.println("Welcome to Employee Wage Computation Problem Part3!");
 		
 		int fullTime = 1;
-
+		int empWagePerHr = 20;
+	    int empHrs = 0;
+	    int empWage = 0; 
+	    
         double empCheck = Math.floor(Math.random()*2);
         System.out.println("Random value for emp Attendence check is: "+ empCheck);
 
         if(empCheck == fullTime ){
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Present.");
+            empHrs = 8;
            }
-       else{
-            System.out.println("Employee is Absent");
+        else{
+            System.out.println("Employee is Absent.");
+            empHrs = 0;
        		}
+        
+        empWage = empHrs*empWagePerHr;
+        System.out.println("Employee Daily Wage is: "+empWage );
 	}     
 }
  
