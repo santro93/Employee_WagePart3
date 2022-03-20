0a3db2c62a75a5eef214631a882d3fced061382f
santro93 <nsantosh435@gmail.com>
2022-03-20 15:54:29 +0530

    Calculated Wages till a condition of total working hours or days is reached for a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,38 +5,46 @@ public class EmployeeWage {
 	public static final int PART_TIME = 2;
 	public static final int EMP_WAGE_PER_HR = 20;
 	public static final int NUM_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problem Part3!");
 		
 		
-	    int empHrs = 0, empWage = 0, totalEmpWage = 0;
+	    int empHrs = 0, empWage = 0, totalEmpWage = 0, totalEmpHrs = 0, totalWorkingDays = 1 ;
 
-	    for (int day = 1; day <= NUM_WORKING_DAYS; day++){; 
+	  //check total emp hour not more than 100 and total days not more than 20
+	    while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_WORKING_DAYS){
 	    
-        double empCheck = Math.floor(Math.random()*3);
-        System.out.println("Random value for emp Attendence check is: "+empCheck);
-
-        switch ((int)empCheck){
-		        case FULL_TIME:
-		        	System.out.println("Employee is present Full Time");
-		        	empHrs = 8;
-		       				break;
-		       
-		        case PART_TIME:
-		       		System.out.println("Employee is present Part Time");
-		       		empHrs = 4;
-		       				break;
-		  
-		        default:
-		        	System.out.println("Employee is Absent");
-		        	empHrs = 0;	
-        	}
+	        double empCheck = Math.floor(Math.random()*3);
+	        System.out.println("Random value for emp Attendence check is: "+empCheck);
+	
+	        switch ((int)empCheck){
+			        case FULL_TIME:
+			        	System.out.println("Employee is present Full Time");
+			        	empHrs = 8;
+			       				break;
+			       
+			        case PART_TIME:
+			       		System.out.println("Employee is present Part Time");
+			       		empHrs = 4;
+			       				break;
+			  
+			        default:
+			        	System.out.println("Employee is Absent");
+			        	empHrs = 0;	
+	        	}
+	        
         empWage = empHrs*EMP_WAGE_PER_HR;
         totalEmpWage += empWage;
+        totalEmpHrs  += empHrs;
         System.out.println("Employee Daily Wage is: "+empWage );
+        
+      //day increment by 1 every iteration
+        totalWorkingDays++ ;
 	 }
-	    System.out.println("Total Employee Wage is: "+totalEmpWage);	
-  }     
+      System.out.println("Total Hours workdone by Employee in month is: "+ totalEmpHrs); 
+      System.out.println("Total Employee Wage is: "+ totalEmpWage); 
+   }     
 }
  
