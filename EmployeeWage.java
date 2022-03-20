1294112996433375f43d8a231ba384126bb2592a
santro93 <nsantosh435@gmail.com>
2022-03-20 15:00:43 +0530

    Added Part time work And it's daily Wage too calculated

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,17 +6,22 @@ public class EmployeeWage {
 		System.out.println("Welcome to Employee Wage Computation Problem Part3!");
 		
 		int fullTime = 1;
+		int partTime = 2;
 		int empWagePerHr = 20;
 	    int empHrs = 0;
 	    int empWage = 0; 
 	    
-        double empCheck = Math.floor(Math.random()*2);
+        double empCheck = Math.floor(Math.random()*3);
         System.out.println("Random value for emp Attendence check is: "+ empCheck);
 
         if(empCheck == fullTime ){
             System.out.println("Employee is Present.");
             empHrs = 8;
            }
+        else if((empCheck == partTime)){
+            System.out.println("Employee is present Part Time.");
+	        empHrs = 4;
+            }
         else{
             System.out.println("Employee is Absent.");
             empHrs = 0;
