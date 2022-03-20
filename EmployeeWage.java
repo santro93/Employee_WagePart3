70b4875ddcdefc1c483a2184bf821d1b02653266
santro93 <nsantosh435@gmail.com>
2022-03-20 14:49:04 +0530

    Employee Present or Absent logic by using Random

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,7 +4,18 @@ public class EmployeeWage {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problem Part3!");
+		
+		int fullTime = 1;
 
-	}
+        double empCheck = Math.floor(Math.random()*2);
+        System.out.println("Random value for emp Attendence check is: "+ empCheck);
+
+        if(empCheck == fullTime ){
+            System.out.println("Employee is Present");
+           }
+       else{
+            System.out.println("Employee is Absent");
+       		}
+	}     
 }
  
